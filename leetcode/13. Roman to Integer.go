04c683d86ac4f1fe6061cc44f76e455252074ec3
saransh-khobragade <saransh98@gmail.com>
2024-02-02 11:57:45 +0530

    roman to int done

--- leetcode/13. Roman to Integer.go
+++ leetcode/13. Roman to Integer.go
@@ -5,64 +5,37 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(romanToInt("I"))
-}
-
 func romanToInt(s string) int {
-	i := 0
-	result := 0
-	for i < len(s) {
-		switch string(s[i]) {
-		case "I":
-			{
-				if i+1 < len(s) && string(s[i+1]) == "V" {
-					result += 4
-					i++
-				} else if i+1 < len(s) && string(s[i+1]) == "X" {
-					result += 9
-					i++
-				} else {
-					result += 1
-				}
-			}
-		case "V":
-			result += 5
-		case "X":
-			{
-				if i+1 < len(s) && string(s[i+1]) == "L" {
-					result += 40
-					i++
-				} else if i+1 < len(s) && string(s[i+1]) == "C" {
-					result += 90
-					i++
-				} else {
-					result += 10
-				}
-			}
-
-		case "L":
-			result += 50
-		case "C":
-			{
-				if i+1 < len(s) && string(s[i+1]) == "D" {
-					result += 400
-					i++
-				} else if i+1 < len(s) && string(s[i+1]) == "M" {
-					result += 900
-					i++
-				} else {
-					result += 100
-				}
-			}
-		case "D":
-			result += 500
-		case "M":
-			result += 1000
-		}
-		i++
+	roman := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
 	}
-	return result
+
+	out := 0
+	prev := ""
+	for i := len(s) - 1; i >= 0; i-- {
+
+		curr := string(s[i])
+
+		if prev == "" {
+			out += roman[curr]
+		} else if roman[curr] >= roman[prev] {
+			out += roman[curr]
+		} else {
+			out -= roman[curr]
+		}
+		prev = curr
+	}
+	return out
+}
+func main() {
+	s := "XLIII" //43
+	fmt.Println(romanToInt(s))
 }
 
 /*
